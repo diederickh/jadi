a4593664e0c015c2a9e9c52899376abe957a5aa9
jacres <james@jamesacres.com>
2012-12-24 03:21:42 -0500

    - windows support to cmakelists - added windows file path and millis() functions - added GLEW + glewExperimental = true for OS X (GLEW is needed to compile on windows so that extensions are visible - OS X does this automatically) - small compatibility fixes for Windows - changed window close to handle clicking the X button in window corner

--- jadi/include/jadi/Jadi.h
+++ jadi/include/jadi/Jadi.h
@@ -10,27 +10,27 @@
 #error "ERROR: Define JADI_PLATFORM = {JADI_LINUX, JADI_OSX, JADI_IOS, JADI_WIN}"
 #endif
 
-#if JADI_PLATFORM==JADI_WIN
+#if JADI_PLATFORM == JADI_WIN
 #define GLEW_STATIC
+#include <Windows.h>
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
+#include <opengl/glew.h>
 #include <opengl/glfw3.h>
 
 #include <jadi/Math.h>
 #include <jadi/OBJ.h>
 #include <jadi/Camera.h>
 
-#if JADI_PLATFORM==JADI_OSX
+#if JADI_PLATFORM == JADI_OSX
 #include <openGL/GL.h> 
 #include <openGL/glext.h>
 #define glGenVertexArrays glGenVertexArraysAPPLE
 #define glBindVertexArray glBindVertexArrayAPPLE
 #endif
-
-
 
 #endif
