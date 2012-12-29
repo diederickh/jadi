1bcdf15db8ba55d38b5b35ea6ec6931d001bd4a2
roxlu <info@roxlu.com>
2012-12-29 22:21:19 +0100

    ..

---
+++ demos/light_rays/src/LightRays.cpp
@@ -0,0 +1,18 @@
+#include "LightRays.h"
+
+LightRays::LightRays() {
+}
+
+LightRays::~LightRays() {
+}
+
+void LightRays::setup() {
+  setupShaders();
+  setupBuffers();
+}
+
+void LightRays::setupShaders() {
+}
+
+void LightRays::setupBuffers() {
+}
